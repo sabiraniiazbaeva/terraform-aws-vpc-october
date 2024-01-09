254255a624b0f2d11804a081f32317fa8a2ce5e2
sabiraniiazbaeva <sabiraniyazbaeva@gmail.com>
2024-01-09 02:40:14 +0000

    New files

---
+++ variable.tf
@@ -0,0 +1,44 @@
+variable region {
+    type = string
+    default = ""
+}
+
+variable vpc_cidr {
+    type = string
+    default = ""
+}
+
+variable subnet1_cidr {
+    type = string
+    default = ""
+}
+
+variable subnet2_cidr {
+    type = string
+    default = ""
+}
+
+variable subnet3_cidr {
+    type = string
+    default = ""
+}
+
+variable vpc_name {
+    type = string
+    default = ""
+}
+
+variable instance_type {
+    type = string
+    default = ""
+}
+
+variable key_name {
+    type = string
+    default = ""
+}
+
+variable type {
+    type = bool
+    default = true
+}
